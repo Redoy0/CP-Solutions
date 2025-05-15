84451006b072b3258b3479a7eb603ac5e95a6ab6
Md Sabbir Ahamed <redoyahamedrs@gmail.com>
2025-05-15 21:51:17 +0600

    New Soln added

---
+++ CF-Codeforces/894 (Div. 3)/1862B.cpp
@@ -0,0 +1,87 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define Fast ios_base :: sync_with_stdio(0);cin.tie(0);
+#define setp setprecision
+#define forn(i,j,n) for(int i=(j);i<n;i++)
+
+#define mod         1000000007
+#define pi          acos(-1.0)
+#define eps         1e-9
+
+#define fs          first
+#define sc          second
+#define all(x)      x.begin(),x.end()
+#define pb(a)       push_back(a)
+#define min_e(v)    *min_element(v.begin(), v.end())
+#define max_e(v)    *max_element(v.begin(), v.end());
+#define mp(a,b)     make_pair(a,b)
+#define sp          cout<<" "
+#define nl          cout<<"\n"
+
+#define set0(a)     memset(a,0,sizeof(a))
+#define setneg(a)   memset(a,-1,sizeof(a))
+#define toLower(s)  transform(s.begin(),s.end(),s.begin(),::tolower)
+#define toUpper(s)  transform(s.begin(),s.end(),s.begin(),::toupper)
+
+
+
+
+#define tc1(x)      cout<<"Case "<<x<<": ";
+#define tc2(x)      cout<<"Case #"<<x<<": ";
+#define tc3(x)      cout<<"Case "<<x<<":\n";
+#define tc4(x)      cout<<"Case #"<<x<<":\n";
+
+#define pr1(x)      cout<<x<<"\n"
+#define pr2(x,y)    cout<<x<<" "<<y<<"\n"
+#define pr3(x,y,z)  cout<<x<<" "<<y<<" "<<z<<"\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int>pii;
+typedef pair<ll, ll>pll;
+typedef pair<double, double>pdd;
+typedef vector<int>vi;
+typedef vector<ll>vll;
+typedef vector<double>vd;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vi b(n);
+    forn(i,0,n) cin>>b[i];
+    vi a;
+    a.pb(b[0]);
+    forn(i,1,n){
+        if(b[i]>=b[i-1]){
+            a.pb(b[i]);
+        }else{
+            a.pb(b[i]);
+            a.pb(b[i]);
+        }
+    }
+    cout<<a.size()<<endl;
+    forn(i,0,a.size()){
+        cout<<a[i];
+        if(i==a.size())
+            cout<<endl;
+        else
+            cout<<" ";
+    }
+
+}
+int main()
+{   Fast;
+    int t=1,tc=1;
+    cin>>t;
+    while(tc<=t){
+        //cout<<"Case "<<tc<<": ";
+        solve();
+
+    tc++;
+    }
+
+    return 0;
+}
